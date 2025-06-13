77e76ea9359ba77f0cd02381bd6da29025a342a2
remorses <beats.by.morse@gmail.com>
2025-06-13 23:28:48 +0200

    use src for react router docs

--- docs-website/react-router.config.ts
+++ docs-website/react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
     ssr: true,
+    appDirectory: 'src',
     // async prerender({ getStaticPaths }) {
     //     return [...getStaticPaths()]
     // },
