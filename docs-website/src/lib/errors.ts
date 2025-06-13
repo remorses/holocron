68c24a8b00d9b21d6357b8763eb559a79779e175
remorses <beats.by.morse@gmail.com>
2025-06-13 23:37:10 +0200

    fixing docs site

---
+++ docs-website/src/lib/errors.ts
@@ -0,0 +1,34 @@
+import { captureException, flush, init } from "sentries";
+
+init({
+  dsn: "",
+  integrations: [],
+  tracesSampleRate: 0.01,
+  profilesSampleRate: 0.01,
+  beforeSend(event) {
+    if (process.env.NODE_ENV === "development") {
+      return null;
+    }
+    if (process.env.BYTECODE_RUN) {
+      return null;
+    }
+    if (event?.["name"] === "AbortError") {
+      return null;
+    }
+
+    return event;
+  },
+});
+
+export async function notifyError(error: any, msg?: string) {
+  console.error(msg, error);
+  captureException(error, { extra: { msg } });
+  await flush(1000);
+}
+
+export class AppError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "AppError";
+  }
+}
