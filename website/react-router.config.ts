4ece7e6cd66cf52342c59e0eabaddcb2161d7306
remorses <beats.by.morse@gmail.com>
2025-07-31 15:22:54 +0200

    prerender

--- website/react-router.config.ts
+++ website/react-router.config.ts
@@ -2,7 +2,7 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
     appDirectory: 'src',
-    // prerender: ['/', '/defer-example'],
+    prerender: ['/'],
     future: {
         // unstable_viteEnvironmentApi: true,
         unstable_optimizeDeps: true,
