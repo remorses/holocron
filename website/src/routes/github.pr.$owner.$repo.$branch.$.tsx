46f3ba0577057e3c6ded36bb7f48f92113c7fcde
remorses <beats.by.morse@gmail.com>
2025-08-23 13:25:01 +0200

    add route to redirect pr urls

---
+++ website/src/routes/github.pr.$owner.$repo.$branch.$.tsx
@@ -0,0 +1,14 @@
+import { redirect } from 'react-router'
+import type { Route } from './+types/github.pr.$owner.$repo.$branch.$'
+
+export async function loader({ params }: Route.LoaderArgs) {
+    const { owner, repo, branch, '*': filePath } = params
+    
+    if (!owner || !repo || !branch || !filePath) {
+        throw new Response('Missing required parameters', { status: 400 })
+    }
+    
+    const githubEditUrl = `https://github.com/${owner}/${repo}/edit/${branch}/${filePath}`
+    
+    throw redirect(githubEditUrl)
+}
