55ea38182345f045dc87728527d2fe807e485e7e
remorses <beats.by.morse@gmail.com>
2025-08-29 14:12:49 +0200

    nn

--- docs-website/scripts/deployment.ts
+++ docs-website/scripts/deployment.ts
@@ -35,7 +35,7 @@ async function main() {
     const port = 7777
 
     const appNameSuffix = basePath ? `-base-path-${basePath.replace(/^\//, '').replace(/\//g, '-')}` : ''
-    const appName = `fumabse-docs-prod${appNameSuffix}`
+    const appName = `fumabase-docs-prod${appNameSuffix}`
 
     await deployFly({
         appName,
