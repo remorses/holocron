ac48c851ad197e359a18d2e22d9b1e8468359f0f
remorses <beats.by.morse@gmail.com>
2025-09-06 18:35:14 +0200

    push prod

--- db/diff.sql
+++ db/diff.sql
@@ -1,6 +1,3 @@
--- CreateEnum
-CREATE TYPE "public"."SiteVisibility" AS ENUM ('private', 'public');
-
 -- AlterTable
-ALTER TABLE "public"."Site" ADD COLUMN     "visibility" "public"."SiteVisibility" NOT NULL DEFAULT 'private';
+ALTER TABLE "public"."Chat" ADD COLUMN     "todos" JSONB NOT NULL DEFAULT '[]';
 
