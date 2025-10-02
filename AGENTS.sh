ea97fc19f3869bf113f75a11430f18798b4949bd
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-10-02 19:44:32 +0200

    more fumadocs docs

--- AGENTS.sh
+++ AGENTS.sh
@@ -7,7 +7,11 @@ NOTICE: AGENTS.md is generated using AGENTS.sh and should NEVER be manually upda
 
 ---
 
+To research fumadocs you can fetch https://fumadocs.dev/llms-full.txt
 
+Optionally grep for what you need with curl https://fumadocs.dev/llms-full.txt | grep 'search term'
+
+You can also search for files in the folder fumadocs to do the same thing
 "
 
 OUTPUT_FILE="AGENTS.md"
