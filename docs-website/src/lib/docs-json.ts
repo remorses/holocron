f7f2ce9dcd2fca96c9b10bcc02afa0e276264041
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-10-11 17:24:43 +0200

    updates

--- docs-website/src/lib/docs-json.ts
+++ docs-website/src/lib/docs-json.ts
@@ -1,8 +1,8 @@
 import { extractNamePathsFromSchema } from 'contesto'
 
-export { DocsConfigSchema, type DocsJsonType, docsJsonSchema } from '@holocron.so/cli/src/docs-json.js'
-export { docsJsonSchema as docsJsonSchemaOriginal } from '@holocron.so/cli/src/docs-json.js'
+export { DocsConfigSchema, type DocsJsonType, docsJsonSchema } from '@holocron.so/cli/src'
 
-const { docsJsonSchema } = await import('@holocron.so/cli/src/docs-json.js')
+
+import { docsJsonSchema } from '@holocron.so/cli/src'
 
 export const exampleNamePathsForDocsJson = extractNamePathsFromSchema(docsJsonSchema as any)
