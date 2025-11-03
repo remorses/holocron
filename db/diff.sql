800059e153ce3caf9a67e971af6bb3dc0a894839
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-11-03 15:36:44 +0100

    Update diff.sql

--- db/diff.sql
+++ db/diff.sql
@@ -2,9 +2,5 @@
 > db@ get-diff /Users/morse/Documents/GitHub/fumabase/db
 > pnpm prisma migrate diff --exit-code --script --from-url $DATABASE_URL_IPV4 --to-schema-datamodel schema.prisma
 
--- AlterTable
-ALTER TABLE "public"."Site" ADD COLUMN     "metadata" JSONB NOT NULL DEFAULT '{}';
+-- This is an empty migration.
 
--- CreateIndex
-CREATE INDEX "Site_metadata_idx" ON "public"."Site" USING GIN ("metadata" jsonb_path_ops);
- ELIFECYCLE  Command failed with exit code 2.
