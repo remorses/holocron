253d3cb7d948eeef25bc43b1dcfda01cc8c0255c
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-11-18 12:44:34 +0100

    diff sql

--- db/diff.sql
+++ db/diff.sql
@@ -2,5 +2,9 @@
 > db@ get-diff /Users/morse/Documents/GitHub/fumabase/db
 > pnpm prisma migrate diff --exit-code --script --from-url $DATABASE_URL_IPV4 --to-schema-datamodel schema.prisma
 
--- This is an empty migration.
+-- CreateEnum
+CREATE TYPE "public"."ChatType" AS ENUM ('hidden', 'website');
 
+-- AlterTable
+ALTER TABLE "public"."Chat" ADD COLUMN     "type" "public"."ChatType" NOT NULL DEFAULT 'website';
+ ELIFECYCLE  Command failed with exit code 2.
