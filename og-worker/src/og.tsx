08a063a4f4e43a1da7cec473fc327e474498db9d
Tommy D. Rossi <beats.by.morse@gmail.com>
2026-05-24 17:33:21 +0200

    Desaturate OG background image to 50%
    Add `filter: saturate(0.5)` on the background `<img>` so colors
    are muted and text remains the focal point.
    
    Session: ses_1a5ecd72bffemUZBc297MgPcJ6

--- og-worker/src/og.tsx
+++ og-worker/src/og.tsx
@@ -89,6 +89,7 @@ function OgTemplate({ iconUrl, title, description, siteName, pageLabel, backgrou
           width: '100%',
           height: '100%',
           objectFit: 'cover',
+          filter: 'saturate(0.5)',
         }}
       />
       <div
